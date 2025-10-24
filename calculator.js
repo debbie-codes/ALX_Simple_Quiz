0a65ed81d5d7bd2ad3949abb423eae5090480df5
Debbie_Codes <deborah.onchari20@gmail.com>
2025-10-24 13:45:22 +0300

    Simple Calculator

---
+++ calculator.js
@@ -0,0 +1,69 @@
+
+// The Arithmetic Functions
+function add(number1, number2){
+    return number1 + number2;
+}
+
+function subtract(number1, number2){
+    return number1 - number2;
+}
+
+function multiply(number1, number2){
+    return number1 * number2;
+}
+
+function division(number1, number2){
+    if(number2 === 0){
+        return "Error: Cannot divide by zero";
+    }
+    return number1 / number2;
+}
+
+
+// Event Listeners for the Arithmetic Functions
+document.getElementById('add').addEventListener('click', function(){
+    const number1 = parseFloat(document.getElementById('number1').value) || 
+    0;
+
+    const number2 = parseFloat(document.getElementById('number2').value) || 
+    0;
+
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('subtract').addEventListener('click', function(){
+    const number1 = parseFloat(document.getElementById('number1').value) || 
+    0;
+
+    const number2 = parseFloat(document.getElementById('number2').value) || 
+    0;
+
+    const result = subtract (number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('multiply').addEventListener('click', function(){
+    const number1 = parseFloat(document.getElementById('number1').value) || 
+    0;
+
+    const number2 = parseFloat(document.getElementById('number2').value) || 
+    0;
+
+    const result = multiply(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('division').addEventListener('click', function(){
+    const number1 = parseFloat(document.getElementById('number1').value) || 
+    0;
+
+    const number2 = parseFloat(document.getElementById('number2').value) || 
+    0;
+
+    const result = division(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+
+
